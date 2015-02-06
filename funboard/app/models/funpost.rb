00841027f1fa3345ae6826165ea2000dd9841727
Isabel <Isabel@1x-vl906-128-119-237-109.wireless.umass.edu>
2015-02-05 21:23:09 -0500

    validates image url

--- funboard/app/models/funpost.rb
+++ funboard/app/models/funpost.rb
@@ -1,4 +1,5 @@
 class Funpost < ActiveRecord::Base
 	validates :title, presence: true
 	validates :url, presence: true, uniqueness: true
+	validates_format_of :url, with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.'
 end
